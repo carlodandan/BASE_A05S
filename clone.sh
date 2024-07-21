123e17d60eaf3291eca96404fe70f0acec3f1d5f
Carlo Dandan <carlodandan.personal@proton.me>
2024-07-21 19:39:04 +0800

    Added submodules pull.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- clone.sh
+++ clone.sh
@@ -2,8 +2,10 @@
 
 ACK="https://android.googlesource.com"
 ACK_BRANCH="master-kernel-build-2022"
+A05S_DIR=$(pwd)
 
 git clone --depth=1 https://github.com/carlodandan/android_kernel_samsung_a05s -b A13 ./kernel_platform/msm-kernel
+cd kernel_platform/msm-kernel; git submodule update --init --recursive; cd ${A05S_DIR}
 git clone --depth=1 ${ACK}/platform/prebuilts/jdk/jdk11 -b ${ACK_BRANCH} ./kernel_platform/prebuilts/jdk/jdk11
 git clone --depth=1 ${ACK}/platform/prebuilts/clang/host/linux-x86 -b ${ACK_BRANCH} ./kernel_platform/prebuilts/clang/host/linux-x86
 git clone --depth=1 ${ACK}/platform/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8 -b ${ACK_BRANCH} ./kernel_platform/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8
